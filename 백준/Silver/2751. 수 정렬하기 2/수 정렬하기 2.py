0edb85484ed04046b78a0ac3ae661aea482fb03c
joosun7227 <96054361+joosun7227@users.noreply.github.com>
2023-07-08 11:47:56 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 816 ms, Memory: 199132 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -1,8 +1,10 @@
-N = int(input())
+import sys
+
+N = int(sys.stdin.readline())
 num =[]
 for i in range(N):
   i -= 1
-  in_put = int(input())
+  in_put = int(sys.stdin.readline())
   num.append(in_put)
 
 num.sort()
