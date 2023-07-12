84975b7668206f689f5238d2d128d9a6faf70e2f
joosun7227 <96054361+joosun7227@users.noreply.github.com>
2023-07-12 23:10:07 +0900

    [Silver V] Title: 날짜 계산, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1476. 날짜 계산/날짜 계산.py
+++ 백준/Silver/1476. 날짜 계산/날짜 계산.py
@@ -1,15 +1,11 @@
-E,S,M,cnt =1,1,1,1
-
-i_E , i_S , i_M = map(int,input().split())
-
-while(True):
-    if i_E==E and i_S==S and i_M==M :
-        break
-    E+=1 ; S+=1 ; M+=1; cnt+=1
-    if E>=16 : E-=15
-    if S>=29 : S-=28
-    if M>=20 : M-=19
-
-
+E,S,M,cnt = 1,1,1,1
+e,s,m = map(int,input().split())
+while (True) :
+  if E==e and S==s and M==m:
+    break
+  E+=1 ; S+=1 ; M+=1 ; cnt+=1
+  if E>=16 : E -=15
+  if S>=29 : S -=28
+  if M>=20 : M -=19
 
 print(cnt)
