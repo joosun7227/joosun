ffcb665a1a7d04881b46d802ff006fa464685ebd
joosun7227 <96054361+joosun7227@users.noreply.github.com>
2024-03-16 17:53:18 +0900

    dalratmart_code
    달랏마트 관련 데이터 처리 코드

---
+++ dalrat/PHPstudy/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h1>WEB</h1>
+  <ol>
+    <li>HTML</li>
+  </ol>
+  <h2>Welcome</h2>
+  Lorem ipsum dolor sit amet, 
+  consectetur adipisicing elit.
+   Saepe, fugiat iure quisquam minus 
+  iste tempora veritatis deleniti repellendus 
+  facilis, maxime ut magni quidem dolorum harum 
+  ab laudantium laborum natus rerum.
+</body>
+</html>
