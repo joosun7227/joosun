946b2ecfc7caba34a4ee604e3f70b83c6f4df038
joosun7227 <96054361+joosun7227@users.noreply.github.com>
2024-06-11 22:07:31 +0900

    자바스크립트2강

---
+++ javascript/1_basics/4_naming_conventions.js
@@ -0,0 +1,13 @@
+// Naming Conventions
+// 1) 일반적으로 영어(라틴문자)를 사용하며 문자와 숫자를 모두 사용할 수 있다.
+// 2) 특수기호는 언더스코어와 달러를 사용할 수 있다.
+// 3) 숫자로 이름을 시작할 수 없다.
+//  1Name, 2Hellow
+// 4) 키워드는 변수명으로 상용할 수 없다.
+let codefactory = '코드팩토리';
+var $ive = '아이브';
+const _yuJin = '안유진';
+
+console.log(codefactory);
+console.log($ive);
+console.log(_yuJin);
